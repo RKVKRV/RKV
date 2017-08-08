7d6edbd71f4494cf6fb1b6cb7017827edb2ab2f7
RKVKRV <kalyta.rom@i.ua>
2017-08-08 18:47:36 +0300

    +

---
+++ project/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE >
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title>skelet</title>
+    <link  rel="stylesheet" type="text/css" href="css/style.css" >
+</head>
+<body>
+<div id = "wrapper">
+    <div class = "page">
+        <header>
+            <div class="logo">
+                Logo
+            </div>
+            <div class = "menu_site">
+                <?php require_once("C:\OpenServer\domains\RKV\lesson 5\menu.php")?>
+            </div>
+        </header>
+        <aside class="product_categories">
+
+        </aside>
+        <div class="content">
+
+            <div class="inner">
+
+                <p>Контент</p>
+                <p>Товары</p>
+                <div class="cont">
+                    <div class="img">
+                        <p style="float: right; font-size: 7px; margin-top: 2px;">09/10/17</p>
+                        <p style=" margin-top :40px; ">изображение товара</p>
+                    </div>
+
+                    <p>название товара</p>
+                    <p style="float:left ">цена</p>
+                    <p style="float:right ">артикул</p>
+
+                        <div class="var">
+                            <p style="font-size: 7px; line-height: 0.4;  ">Название варианта, цена варианта</p>
+
+                        </div>
+
+                </div>
+                <div class="cont">
+                    <div class="img">
+                        <p style="float: right; font-size: 7px; margin-top: 2px;">09/10/17</p>
+                        <p style=" margin-top :40px; ">изображение товара</p>
+                    </div>
+
+                    <p>название товара</p>
+                    <p style="float:left ">цена</p>
+                    <p style="float:right ">артикул</p>
+
+                    <div class="var">
+                        <p style="font-size: 7px; line-height: 0.4;  ">Название варианта, цена варианта</p>
+
+                    </div>
+
+                </div>
+                <div class="cont">
+                    <div class="img">
+                        <p style="float: right; font-size: 7px; margin-top: 2px;">09/10/17</p>
+                        <p style=" margin-top :40px; ">изображение товара</p>
+                    </div>
+
+                    <p>название товара</p>
+                    <p style="float:left ">цена</p>
+                    <p style="float:right ">артикул</p>
+
+                    <div class="var">
+                        <p style="font-size: 7px; line-height: 0.4;  ">Название варианта, цена варианта</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <footer>
+            <p>Футер (подвал) сайта</p>
+        </footer>
+    </div>
+</div>
+</body>
+</html>
+
